4db6c17e72a4275d919c91e67371b5010eecfa91
Ryan Pepito <pepito.ryan@gmail.com>
2019-08-30 17:31:29 -0700

    Deleted opendialog object and just made opendialog within button click event.

--- Photo Sorter/Form1.cs
+++ Photo Sorter/Form1.cs
@@ -32,10 +32,34 @@ namespace Photo_Sorter
         private void BrowseButton_Click(object sender, EventArgs e)
         {
             //Testing button functions
+            /*
             DialogResult testResult = MessageBox.Show("Browse Test Successful! Click OK to close.");
             if (testResult == DialogResult.OK)
             {
                 this.Close();
+            }
+            */
+            try
+            {
+                OpenFileDialog imgObj = new OpenFileDialog();
+
+                //filters that object to open certain image files
+                //Version 1.0: Support for ARW files so far 
+                imgObj.Filter = "Image Files (*.jpg;*.jpeg;*.arw)|*.jpg;*.jpeg;*.arw";
+                imgObj.Title = "Selecting files...";
+
+                //Setting initial directory to SD card
+
+                imgObj.Multiselect = true;
+                //if the file dialog opens, grab the location of the image and its filename
+                if (imgObj.ShowDialog() == DialogResult.OK)                                                            
+                {
+                    FilePathText.Text = imgObj.FileName;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -48,5 +72,6 @@ namespace Photo_Sorter
         {
 
         }
+
     }
 }
